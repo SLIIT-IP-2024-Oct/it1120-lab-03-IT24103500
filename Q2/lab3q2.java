6046d2bc38719f60b454372f6f636b86d0b8e8b2
IT24103500 <it24103500@my.sliit.lk>
2024-11-01 10:27:24 +0530

    q2

---
+++ Q2/lab3q2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Lab3Q1{
+     public static void main(String []args){
+	double salary;
+	double othours;
+	double y;
+	double otamount;
+	double rate;
+	
+	Scanner input = new Scanner(System.in);
+	System.out.println("Enter salary :");
+	salary=input.nextDouble();
+
+	System.out.println("Enter ot hours :");
+	othours=input.nextDouble();
+
+	System.out.println("Enter ot hours rate :");
+	rate=input.nextDouble();
+
+
+	otamount=othours*rate;
+	salary=otamount+salary;
+	
+	
+	System.out.println("Final salary : "+ salary);
+	
+
+}
+}
